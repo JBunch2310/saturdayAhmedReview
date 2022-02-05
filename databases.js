3c951885a8af3a8037a6c848bcaad10809b94546
Jamie Bunch <JamieBunch@ymail.com>
2022-02-05 16:53:30 -0500

    update

---
+++ databases.js
@@ -0,0 +1,11 @@
+/**
+ * 0. npm init -y
+ * 1. Install Firebase Packages
+ * 2. Import Firebase
+ * 3. Connect to the database
+ * 4. Access the collections
+ * 5. Read Documents
+ * 6. Insert Documents
+ * 7. Edit/Update Documents
+ */
+
